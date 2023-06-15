75182e87c5d1ac6beb6b4d3f5c6b7ffd800f2282
niloofar sadeghi <niloofar.sadeghi@re-work.dev>
2023-06-15 11:55:56 +0330

    fix: give feedback button in notification not navigating to order page when user is outside p2p

--- packages/p2p/src/components/app.jsx
+++ packages/p2p/src/components/app.jsx
@@ -208,8 +208,9 @@ const App = () => {
         }
         setP2POrderProps({
             order_id,
-            setP2POrderTab: general_store.setP2POrderTab,
-            setIsRatingModalOpen: order_store.setIsRatingModalOpen,
+            redirectToOrderDetails: general_store.redirectToOrderDetails,
+            setIsRatingModalOpen: is_open =>
+                is_open ? general_store.showModal({ key: 'RatingModal' }) : general_store.hideModal(),
             navigateToOrderDetails,
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
--- packages/p2p/src/components/order-details/order-details.jsx
+++ packages/p2p/src/components/order-details/order-details.jsx
@@ -110,17 +110,18 @@ const OrderDetails = observer(() => {
             handleChatChannelCreation();
         }
 
+        setP2POrderProps({
+            order_id: order_store.order_id,
+            setP2POrderTab: general_store.setP2POrderTab,
+            setIsRatingModalOpen: is_open => (is_open ? showRatingModal() : hideModal()),
+        });
+
         return () => {
             disposeListeners();
             disposeReactions();
             order_store.setOrderPaymentMethodDetails(undefined);
             order_store.setOrderId(null);
             order_store.setActiveOrder(null);
-            setP2POrderProps({
-                order_id: order_store.order_id,
-                setP2POrderTab: general_store.setP2POrderTab,
-                setIsRatingModalOpen: is_open => (is_open ? showRatingModal : hideModal),
-            });
             history.replace({
                 search: '',
                 hash: location.hash,
